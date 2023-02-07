31a546a22dab21f0f10a178e79519c05cd0eb367
laptinho <camsanbar@gmail.com>
2023-02-07 18:29:56 -0300

    Add: anadido controladores de update y destroy

--- controllers/purchase.controller.js
+++ controllers/purchase.controller.js
@@ -36,6 +36,45 @@ const controller = {
     } catch (error) {
       next(error)
     }
+  },
+  my_details: async ( req, res, next ) => {
+    let query = {}
+    const { id } = req.params
+    query._id = id
+    query.buyer_id = "Soy un id"
+    try {
+      const purchase = await Purchase.findOne(query)
+      req.body.success = true
+      req.body.sc = 200
+      req.body.data = purchase
+      return defaultResponse(req, res)
+    } catch (error) {
+      next(error)
+    }
+  },
+  destroy: async ( req, res, next ) => {
+    const { id } = req.params
+    try {
+      await Purchase.findByIdAndDelete(id)
+      req.body.success = true
+      req.body.sc = 200
+      req.body.data = 'deleted'
+      return defaultResponse(req, res)
+    } catch (error) {
+      next(error)
+    }
+  },
+  update: async ( req, res, next ) => {
+    const { id } = req.params
+    try {
+      await Purchase.findByIdAndUpdate(id, req.body)
+      req.body.success = true
+      req.body.sc = 200
+      req.body.data = 'updated'
+      return defaultResponse(req, res)
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
--- routes/purchase.js
+++ routes/purchase.js
@@ -4,10 +4,13 @@ import schema from '../schemas/purchase.schema.js';
 import validator from '../middlewares/validator.js'
 import controller from "../controllers/purchase.controller.js";
 
-const { create, my_purchases, read } = controller
+const { create, my_purchases, read, my_details, destroy, update } = controller
 
 router.get('/', read)
 router.post('/', validator(schema), create)
 router.get('/me', my_purchases)
+router.get('/:id', my_details)
+router.delete('/:id', destroy)
+router.put('/:id', update)
 
 export default router
