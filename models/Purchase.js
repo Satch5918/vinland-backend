b188d52403fe7075008cf9077c71c8ebdcc27847
laptinho <camsanbar@gmail.com>
2023-02-07 13:39:12 -0300

    Add: anadido modelo purchase e importacion de dotenv

---
+++ models/Purchase.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+	{
+		buyer_id: { type: mongoose.Types.ObjectId, require: true },
+		status: { type: String, require: true },
+		products: { type: mongoose.Types.Array, require: true },
+		total: { type: Number, require: true },
+	},
+	{
+		timestamps: true,
+	}
+);
+
+export const Purchase = mongoose.model('purchases', schema)
