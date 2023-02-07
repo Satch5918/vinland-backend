1aa1ab03ca559bdc04491501fa364197a65cf437
laptinho <camsanbar@gmail.com>
2023-02-07 18:06:14 -0300

    TODO: usar ids de usuarios compradores mas adelante

--- controllers/purchase.controller.js
+++ controllers/purchase.controller.js
@@ -13,6 +13,18 @@ const controller = {
     } catch (error) {
       next(error)
     }
+  },
+  my_purchases: async ( req, res, next ) => {
+    req.body.buyer_id = "Soy un id"
+    try {
+      const purchases = await Purchase.find({ buyer_id: req.body.buyer_id })
+      req.body.success = true
+      req.body.sc = 200
+      req.body.data = purchases
+      return defaultResponse(req, res)
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
--- routes/purchase.js
+++ routes/purchase.js
@@ -4,8 +4,9 @@ import schema from '../schemas/purchase.schema.js';
 import validator from '../middlewares/validator.js'
 import controller from "../controllers/purchase.controller.js";
 
-const { create } = controller
+const { create, my_purchases } = controller
 
 router.post('/', validator(schema), create)
+router.get('/me', my_purchases)
 
 export default router
