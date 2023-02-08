fe164bbf6ec0003680b4935f5cea0e223cf6276a
BstivenCruz <bseguracruz@yahoo.com>
2023-02-08 00:42:36 -0500

    agregue ruta de product

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import express, {Router} from 'express';
-
 let router = express.Router();
+import product from './product.js'
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('Vinland server ready');
 });
+
+
+router.use('/product',product)
 
 export default router;
