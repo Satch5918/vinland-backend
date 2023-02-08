d14e1c13994e936fff0c1c06fcc5de84c1cf71d0
BstivenCruz <bseguracruz@yahoo.com>
2023-02-08 12:19:53 -0500

    controladores all one best ultimos

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,7 +5,7 @@ const controller = {
   create: async (req, res, next) => {
     try {
       const {
-        gender,
+        genre,
         name,
         photo,
         price,
@@ -17,7 +17,7 @@ const controller = {
         sales,
       } = req.body;
       await Product.create({
-        gender,
+        genre,
         name,
         photo,
         price,
@@ -60,6 +60,68 @@ const controller = {
       next(error);
     }
   },
+  read_All: async (req, res, next) => {
+    try {
+      const products = await Product.find().populate(
+        "genre",
+        "name -_id"
+      ).select(
+        "-_id -demo -sales  -description -createdAt -updatedAt -__v"
+      )
+      req.body.success = true;
+      req.body.sc = 201;
+      req.body.data = products;
+      return defaultResponse(req, res);
+    } catch (error) {
+      next(error);
+    }
+  },
+  read_one: async (req, res) => {
+    const { id } = req.params;
+    try {
+      let products = await Product.findById(id, "-_id -demo -sales  -description -createdAt -updatedAt -__v").populate(
+        "genre",
+        "name -_id"
+      );
+      req.body.success = true;
+      req.body.sc = 201;
+      req.body.data = products;
+      return defaultResponse(req, res);
+    } catch (error) {
+      next(error);
+    }
+  },
+  read_bestseller: async (req, res, next) => {
+    try {
+      const products = await Product.find().populate(
+        "genre",
+        "name -_id"
+      ).select(
+        "-_id -demo -sales  -description -createdAt -updatedAt -__v"
+      ).sort('-sales').limit(2)
+      req.body.success = true;
+      req.body.sc = 201;
+      req.body.data = products;
+      return defaultResponse(req, res);
+    } catch (error) {
+      next(error);
+    }
+  },
+  read_ultimos: async (req, res, next) => {
+    try {
+      const products = await Product.find().populate(
+        "genre",
+        "name -_id"
+      ).select(
+        "-_id -demo -sales  -description -createdAt -updatedAt -__v"
+      ).sort('-createdAt').limit(2)
+      req.body.success = true;
+      req.body.sc = 201;
+      req.body.data = products;
+      return defaultResponse(req, res);
+    } catch (error) {
+      next(error);
+    }
+  },
 };
-
 export default controller;
