3d08a47f147aee3fcd74f4d251da75051b225659
BstivenCruz <bseguracruz@yahoo.com>
2023-02-08 12:27:18 -0500

    agrego las rutas con sus controladores

--- routes/product.js
+++ routes/product.js
@@ -4,16 +4,17 @@ import validator from '../middlewares/validator.js';
 import product from '../schemas/product.schema.js';
 import isAdmin from '../middlewares/isAdmin.js';
 import controller from '../controllers/product.controller.js';
-const {create , updated , destroyed} = controller
+const {create , updated , destroyed , read_All , read_one , read_bestseller , read_ultimos} = controller
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-  });
 
 router.post('/',/* isAdmin */ validator(product),create)
 router.put('/:id',/* isAdmin */updated)
 router.delete('/:id',destroyed)
+router.get("/",read_All)
+router.get("/best",read_bestseller)
+router.get("/ultimos",read_ultimos)
+router.get("/:id",read_one)
+
 
 
 
