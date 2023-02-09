633c8b592ab8f8823e4af6827f8e62f8dadc237b
laptinho <camsanbar@gmail.com>
2023-02-09 14:12:33 -0300

    Fix: anadido passport a rutas de productos

--- routes/product.js
+++ routes/product.js
@@ -7,12 +7,12 @@ import controller from '../controllers/product.controller.js';
 const {create , updated , destroyed , read_all , read_one , read_bestseller , read_last} = controller
 
 
-router.post('/', isAdmin, validator(product),create)
+router.post('/', passport.authenticate('jwt', { session:false }), isAdmin, validator(product),create)
 router.get("/",read_all)
 router.get("/best",read_bestseller)
 router.get("/last",read_last)
-router.put('/:id', isAdmin, updated)
-router.delete('/:id',destroyed)
+router.put('/:id', passport.authenticate('jwt', { session:false }), isAdmin, updated)
+router.delete('/:id', passport.authenticate('jwt', { session:false }), isAdmin, destroyed)
 router.get("/:id",read_one)
 
 
