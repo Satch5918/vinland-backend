d67e8f22831cfb6f96c5b000bfe11f4a3eeae2e8
laptinho <camsanbar@gmail.com>
2023-02-09 14:11:06 -0300

    Fix: Arreglada consistencia de los productos

--- routes/product.js
+++ routes/product.js
@@ -4,15 +4,15 @@ import validator from '../middlewares/validator.js';
 import product from '../schemas/product.schema.js';
 import isAdmin from '../middlewares/isAdmin.js';
 import controller from '../controllers/product.controller.js';
-const {create , updated , destroyed , read_All , read_one , read_bestseller , read_ultimos} = controller
+const {create , updated , destroyed , read_all , read_one , read_bestseller , read_last} = controller
 
 
-router.post('/',/* isAdmin */ validator(product),create)
-router.put('/:id',/* isAdmin */updated)
-router.delete('/:id',destroyed)
-router.get("/",read_All)
+router.post('/', isAdmin, validator(product),create)
+router.get("/",read_all)
 router.get("/best",read_bestseller)
-router.get("/ultimos",read_ultimos)
+router.get("/last",read_last)
+router.put('/:id', isAdmin, updated)
+router.delete('/:id',destroyed)
 router.get("/:id",read_one)
 
 
